7349d0a90b84258e81752686c60b2d4da9ac4acf
manavisharma14 <manavisharma14@gmail.com>
2026-06-08 19:03:41 -0500

    search api over http

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"net/http"
+		"strings"
+	)
+
+var index = make(map[string][]string)
+var docs []Document
+
+func helloHandle(w http.ResponseWriter, r *http.Request){
+
+	query := r.URL.Query().Get("q")
+	results := rankResults(index, query)
+
+	for _, result := range results {
+		for _, doc := range docs {
+			if doc.ID == result.ID {
+				fmt.Fprintln(w, doc.Text, result.Score)
+			}
+		}
+	}
+}
 
 func main(){
-	fmt.Println("Search engine running ...")
+
+	docs = []Document{
+		{ID: "1", Text: "go go go is fast"},
+		{ID: "2", Text: "go is simple"},
+		{ID: "3", Text: "search engines use indexes"},
+	}
+
+	for _, doc := range docs{
+		words := strings.Fields(doc.Text)
+
+		for _, word := range words {
+			index[word] = append(index[word], doc.ID)
+		}
+	}
+
+	http.HandleFunc("/", helloHandle)
+	
+	fmt.Println("server running on :8080")
+
+	http.ListenAndServe(":8080", nil)
 }
