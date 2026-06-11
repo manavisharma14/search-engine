d871ad32716b8cc9649367dcd19619680e7bc797
manavisharma14 <manavisharma14@gmail.com>
2026-06-11 14:40:57 -0500

    searchHandler shard/main.go

--- shard/main.go
+++ shard/main.go
@@ -5,10 +5,15 @@ import (
 	"net/http"
 )
 
+func searchHandler(w http.ResponseWriter, r *http.Request){
+	query := r.URL.Query().Get("q")
+	fmt.Println(w, "shard received query: %s", query)
+}
+
 func main(){
 	http.handleFunc("/search", searchHandler)
 	
 	fmt.Println("shard server running :5001")
-	
+
 	http.ListenAndServe(":5001", nil)
 }
