a0aad1b9e27655749c2edfe404ca96574ff7bdc6
manavisharma14 <manavisharma14@gmail.com>
2026-06-11 14:50:06 -0500

    func main() /coordinator/main.go

--- coordinator/main.go
+++ coordinator/main.go
@@ -5,3 +5,17 @@ import (
 	"io"
 	"net/http"
 )
+
+func main(){
+	resp, err := http.Get(
+		"http://localhost:5001/search?q=grpc",
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(body))
+}
