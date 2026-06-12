c1cf156be068cacd28a256a35aa2e3287a2edcfe
manavisharma14 <manavisharma14@gmail.com>
2026-06-11 21:22:23 -0500

    shard2 main.go

--- shard2/main.go
+++ shard2/main.go
@@ -1,12 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
 
 )
 
+type SearchResult struct {
+	ID		string
+	Score	int
+}
+
+var index = map[string][]string{
+	"redis": {"3"},
+	"cache": {"3"},
+	"docker": {"4"},
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request){
+	scores := make(map[string]int)
+	query := r.URL.Query().Get("q")
+	words := strings.Fields(query)
+
+
+	for _, word := range words {
+		ids := index[word]
+		for _, id := range ids {
+			scores[id]++
+		}
+	}
+	fmt.Println(scores)
+
+	results := []SearchResult{}
+
+	for id, score := range scores {
+		results = append(results, SearchResult{
+			ID: id,
+			Score: score,
+		})
+	}
+
+	json.NewEncoder(w).Encode(results)
+}
+
 func main(){
+	http.HandleFunc("/search", searchHandler)
 	fmt.Println("shard server running on :5002")
 	http.ListenAndServe(":5002", nil)
 }
